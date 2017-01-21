0e51db9b0566c01e547bad288c28a088659ef4fe
Luis Tejada <Luis Tejada@LUISTEJADA-PC>
2017-01-21 00:40:56 -0400

    Luis Tejada

---
+++ problema3/problema3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problema3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String fecha = String.Format("La fecha y hora es: {0: hh:mm:sstt dd/MM/yy}", DateTime.Now);
+            Console.WriteLine(fecha);
+            Console.ReadKey();
+        }
+    }
+}
